29ce6044244dedfe7fca64ff16021342a2d2e9b1
Ahmad AR <38329562+Ahmadar91@users.noreply.github.com>
2019-10-22 13:41:52 +0200

    setting up

---
+++ examination-3/src/MemoryGame/MemoryGame.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './game.css'
+export default class MemoryGame extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>
+                     Hello World!
+        </h1>
+      </div>
+    )
+  }
+}
---
+++ examination-3/src/chat/chat.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './chat.css'
+
+export default class Chat extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>
+                   Hello World!
+        </h1>
+      </div>
+    )
+  }
+}
