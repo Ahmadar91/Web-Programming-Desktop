4e5c2808cea2a3549ad12bd515be9341034b00ca
Ahmad AR <38329562+Ahmadar91@users.noreply.github.com>
2019-10-24 20:52:06 +0200

    fixes

--- examination-3/src/chat/chat.jsx
+++ examination-3/src/chat/chat.jsx
@@ -5,17 +5,7 @@ import Draggable from 'react-draggable' // The default
 export default class Chat extends React.Component {
   render () {
     return (
-      <Draggable
-        axis='both'
-        handle='.handle'
-        defaultPosition={{ x: 0, y: 0 }}
-        position={null}
-        grid={[25, 25]}
-        scale={1}
-        onStart={this.handleStart}
-        onDrag={this.handleDrag}
-        onStop={this.handleStop}
-      >
+      <Draggable>
         <div id='window' className='handle'>
           <div className='header'> Live Chat <button className='close'>x</button></div>
           <div className='body'> Todo Live Chat <div> Lorem ipsum </div></div>
