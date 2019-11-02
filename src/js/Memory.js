fd526714651d44f4822ab021f7bffb72aa66dc28
Ahamd <aa224fn@student.lnu.se>
2019-11-02 14:27:08 +0100

    mg done

--- src/js/Memory.js
+++ src/js/Memory.js
@@ -5,52 +5,84 @@ export function Memory (rows, cols, container) {
   let turn1
   let turn2
   let lastTile
+  let pairs = 0
+  let tries = 0
   tiles = getPictureArray(rows, cols)
-  const template = document.querySelectorAll('#memoryContainer template')[0].content.firstElementChild
+  const templateDiv = document.querySelectorAll('#memoryContainer template')[0].content.firstElementChild
+  const div = document.importNode(templateDiv, false)
+
   tiles.forEach(function (element, index) {
-    a = document.importNode(template, true)
-    imgContainer.appendChild(a)
-    a.addEventListener('click', function (event) {
-      const img = event.target.nodeName === 'IMG' ? event.target : event.target.firstElementChild
-      turnBrick(element, index, img)
-    })
+    a = document.importNode(templateDiv.firstElementChild, true)
+    a.firstElementChild.setAttribute('data-brickNumber', index)
+    div.appendChild(a)
 
     if ((index + 1) % cols === 0) {
-      imgContainer.appendChild(document.createElement('br'))
+      div.appendChild(document.createElement('br'))
     }
   })
+  div.addEventListener('click', function (event) {
+    event.preventDefault()
+    const img = event.target.nodeName === 'IMG' ? event.target : event.target.firstElementChild
+    const index = parseInt(img.getAttribute('data-brickNumber'))
+    turnBrick(tiles[index], index, img)
+  })
+
+  imgContainer.appendChild(div)
   function turnBrick (tile, index, img) {
+    if (turn2) {
+      return
+    }
+
     img.src = `../image/${tile}.png`
 
     if (!turn1) {
       turn1 = img
       lastTile = tile
     } else {
+      if (img === turn1) {
+        return
+      }
+      tries += 1
+      console.log(tries)
+
       turn2 = img
+
       if (tile === lastTile) {
         console.log('pair!')
+        pairs++
+        if (pairs === (rows * cols) / 2) {
+          console.log('Won! Number of tries: ' + tries)
+        }
+        setTimeout(() => {
+          turn1.parentElement.classList.add('remove')
+          turn2.parentElement.classList.add('remove')
+          turn1 = null
+          turn2 = null
+        }, 300)
       } else {
-        turn1.src = '../image/0.png'
-        turn2.src = '../image/0.png'
+        setTimeout(() => {
+          turn1.src = '../image/0.png'
+          turn2.src = '../image/0.png'
+          turn1 = null
+          turn2 = null
+        }, 500)
       }
-      turn1 = null
-      turn2 = null
     }
   }
-  function getPictureArray (rows, cols) {
-    let index
-    const arr = []
-    for (index = 1; index <= (rows * cols) / 2; index += 1) {
-      arr.push(index)
-      arr.push(index)
-    }
+}
+function getPictureArray (rows, cols) {
+  let index
+  const arr = []
+  for (index = 1; index <= (rows * cols) / 2; index += 1) {
+    arr.push(index)
+    arr.push(index)
+  }
 
-    for (let i = arr.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1))
-      const temp = arr[i]
-      arr[i] = arr[j]
-      arr[j] = temp
-    }
-    return arr
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    const temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
   }
+  return arr
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,7 @@
 import { Memory } from './Memory.js'
 
-Memory(4, 4, 'memoryContainer')
+// Memory(4, 4, 'memoryContainer')
+
+Memory(2, 2, 'memoryContainer')
+
+// Memory(2, 4, 'memoryContainer')
