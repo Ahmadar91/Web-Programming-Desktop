5c2d995135efc9dcd1333907ba4ef7c3067d2e9b
Ahamd <aa224fn@student.lnu.se>
2019-11-02 14:16:44 +0100

    mg half

--- src/js/Memory.js
+++ src/js/Memory.js
@@ -1,15 +1,35 @@
 export function Memory (rows, cols, container) {
   let img
-  const imgContainer = document.querySelector('#' + container)
-  let index
-  for (index = 0; index < rows * cols; index++) {
-    img = document.createElement('img')
-    img.setAttribute('src', '../image/0.png')
+  const imgContainer = document.querySelector(`#${container}`)
+  let tiles = []
+  tiles = getPictureArray(rows, cols)
+  const template = document.querySelectorAll('#memoryContainer template')[0].content.firstElementChild
+  tiles.forEach(function (element, index) {
+    img = document.importNode(template, true)
     imgContainer.appendChild(img)
-  }
-  img = document.createElement('img')
-  img.setAttribute('src', '../image/0.png')
-  if (index + 1 % cols === 0) {
-    document.appendChild(document.createElement('br'))
+    img.addEventListener('click', function () {
+      console.log(index)
+    })
+
+    if ((index + 1) % cols === 0) {
+      imgContainer.appendChild(document.createElement('br'))
+    }
+  })
+
+  function getPictureArray (rows, cols) {
+    let index
+    const arr = []
+    for (index = 1; index <= (rows * cols) / 2; index += 1) {
+      arr.push(index)
+      arr.push(index)
+    }
+
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1))
+      const temp = arr[i]
+      arr[i] = arr[j]
+      arr[j] = temp
+    }
+    return arr
   }
 }
