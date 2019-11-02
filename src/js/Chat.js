ec9c35b03457594e02983d97d6ece1db77fc296c
Ahmad AR <38329562+Ahmadar91@users.noreply.github.com>
2019-11-02 14:39:37 +0100

    socket working

---
+++ src/js/Chat.js
@@ -0,0 +1,48 @@
+
+export function chat () {
+  const chatContainer = document.querySelector('#ChatContainer')
+  const templateDiv = document.querySelectorAll('#ChatContainer template')[0].content.firstElementChild
+
+  const chatDiv = document.importNode(templateDiv, true)
+  console.log(chatDiv)
+  chatContainer.appendChild(chatDiv)
+  const messages = document.querySelector('#messages')
+  const textBox = document.querySelector('#textBox')
+  const button = document.querySelector('#button')
+  button.addEventListener('click', () => {
+    sendText()
+    textBox.value = ''
+  })
+  const socket = new window.WebSocket('ws://vhost3.lnu.se:20080/socket/')
+
+  function sendText () {
+    if (textBox.value === '') {
+      return
+    }
+    const msg = {
+      type: 'message',
+      data: textBox.value,
+      username: 'test222',
+      channel: 'iiiii',
+      key: 'eDBE76deU7L0H9mEBgxUKVR0VCnq0XBd'
+    }
+
+    socket.send(JSON.stringify(msg))
+  }
+
+  socket.onmessage = function (event) {
+    console.log(event.data)
+    var text = ''
+    var msg = JSON.parse(event.data)
+
+    if (msg.type === 'message') {
+      text = msg.username + ': ' + msg.data
+    }
+
+    if (text.length) {
+      const newMsg = document.createElement('li')
+      newMsg.textContent = text
+      messages.appendChild(newMsg)
+    }
+  }
+}
--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,26 @@
 import { Memory } from './Memory.js'
+import { chat } from './Chat.js'
 
 // Memory(4, 4, 'memoryContainer')
 
-Memory(2, 2, 'memoryContainer')
+// Memory(2, 2, 'memoryContainer')
 
 // Memory(2, 4, 'memoryContainer')
+const bar = document.querySelector('#nav-mobile')
+export function handleClick () {
+  console.log()
+  bar.addEventListener('click', (event) => {
+    switch (event.target.id) {
+      case 'chat':
+        chat()
+        console.log('Chat')
+        break
+      case 'MG' :
+        Memory(2, 2, 'memoryContainer')
+        console.log('MG')
+        break
+    }
+  })
+}
+handleClick()
+chat()
